dde4bfaecaaec1dffcb839baae1bce375a85a6fe
zhikuanaw <xiaoichiban@hotmail.com>
2020-12-07 23:22:54 +0800

    add vuex const, actions, mutations and get for cart

---
+++ client/store/index.js
@@ -0,0 +1,52 @@
+export const state = () => ({
+  // State
+  cart: [],
+  cartLength: 0
+});
+
+/*  state = state variable
+    commit = action to be called
+    product = product supplied from FE
+*/
+
+export const actions = {
+  addProductToCart({ state, commit }, product) {
+    const cartProduct = state.cart.find(prod => prod._id === product._id);
+
+    if (!cartProduct) {
+      commit("pushProductToCart", product);
+    } else {
+      commit("incrementProductQty", product);
+    }
+
+    commit("incrementCartLength");
+  }
+};
+
+export const mutations = {
+  pushProductToCart(state, product) {
+    product.quantity = 1;
+    state.cart.push(product);
+  },
+
+  incrementProductQty(state, product) {
+    product.quantity++;
+    let indexOfProduct = state.cart.indexOf(product);
+    state.cart.splice(indexOfProduct, 1, product); //replace old object with new updated object
+  },
+
+  incrementCartLength(state) {
+    state.cartLength = 0;
+    if (state.cart.length > 0) {
+      state.cart.map(product => {
+        state.cartLength += product.quantity;
+      });
+    }
+  }
+};
+
+export const getters = {
+  getCartLength(state) {
+    return state.cartLength;
+  }
+};
