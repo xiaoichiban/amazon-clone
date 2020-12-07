600c31da8a76928ee9c1dcbb6283f4fc5fb396f4
zhikuanaw <xiaoichiban@hotmail.com>
2020-12-07 17:32:31 +0800

    add star ratings and configuration for client

---
+++ client/plugins/vue-star-rating.js
@@ -0,0 +1,4 @@
+import Vue from "vue";
+import VueStarRating from "vue-star-rating";
+
+Vue.component("vue-star-rating", VueStarRating); //<--- the name you used to register the plugin will be the same to use when in the component (vue-star-rating)
