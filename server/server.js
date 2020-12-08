d3bd3fa70e90fa57d9817ab63083eb46f56aac18
zhikuanaw <xiaoichiban@hotmail.com>
2020-12-08 15:55:00 +0800

    add order data retrival logic backend

--- server/server.js
+++ server/server.js
@@ -43,6 +43,7 @@ const userRoutes = require("./routes/auth");
 const reviewRoutes = require("./routes/review");
 const addressRoutes = require("./routes/address");
 const paymentRoutes = require("./routes/payment");
+const orderRoutes = require("./routes/order");
 
 app.use("/api", productRoutes);
 app.use("/api", categoryRoutes);
@@ -51,6 +52,7 @@ app.use("/api", userRoutes);
 app.use("/api", reviewRoutes);
 app.use("/api", addressRoutes);
 app.use("/api", paymentRoutes);
+app.use("/api", orderRoutes);
 
 app.listen(3000, (err) => {
   if (err) {
