da4366b501dbc2c4ce0a29e132dc5bdf9905d696
zhikuanaw <xiaoichiban@hotmail.com>
2020-12-08 11:58:36 +0800

    add shipping for placeorder page

--- client/store/index.js
+++ client/store/index.js
@@ -1,7 +1,9 @@
 export const state = () => ({
   // State
   cart: [],
-  cartLength: 0
+  cartLength: 0,
+  shippingPrice: 0,
+  shippingEstimatedDelivery: ""
 });
 
 /*  state = state variable
@@ -72,6 +74,11 @@ export const mutations = {
     state.cartLength -= product.quantity;
     let indexOfProduct = state.cart.indexOf(product);
     state.cart.splice(indexOfProduct, 1);
+  },
+
+  setShipping(state, { price, estimatedDelivery }) {
+    state.shippingPrice = price;
+    state.shippingEstimatedDelivery = estimatedDelivery;
   }
 };
 
@@ -89,5 +96,13 @@ export const getters = {
     });
 
     return total;
+  },
+  getCartTotalPriceWithShipping(state) {
+    let total = 0;
+    state.cart.map(product => {
+      total += product.price * product.quantity;
+    });
+
+    return total + state.shippingPrice;
   }
 };
